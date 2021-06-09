b260a9cafea6d3e1f5fd185130c102f0f64a2d9e
Dalton Menezes <daltonmenezes@outlook.com>
2021-06-09 17:56:28 -0300

    Criando Componentes com o Styled Components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles'
+
+export function Dashboard(){
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  )
+}
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: purple;
+`;
