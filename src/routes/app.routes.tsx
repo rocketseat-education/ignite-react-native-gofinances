b992e96ae9fe30897387a1648dd374d3dbb5bac7
Dalton Menezes <daltonmenezes@outlook.com>
2021-06-16 17:14:11 -0300

    Estilizando a Tab Navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Platform } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { Dashboard } from '../screens/Dashboard';
@@ -7,16 +10,60 @@ import { Register } from '../screens/Register';
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes(){
+  const theme = useTheme();
+
   return(
-    <Navigator>
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.secondary,
+        inactiveTintColor: theme.colors.text,
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 88
+        }
+      }}
+    >
       <Screen
         name="Listagem"
         component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ))
+        }}
       />
 
       <Screen
         name="Cadastrar"
         component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name="attach-money"
+              size={size}
+              color={color}
+            />
+          ))
+        }}
+      />
+
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name="pie-chart"
+              size={size}
+              color={color}
+            />
+          ))
+        }}
       />
     </Navigator>
   );
