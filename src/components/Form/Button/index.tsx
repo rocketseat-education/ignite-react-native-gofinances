7e96604cbf93354a998e1aa7152ac34163bbc453
Dalton Menezes <daltonmenezes@outlook.com>
2021-06-16 22:01:57 -0300

    Tipando o Button

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -5,11 +5,16 @@ import { Container, Title } from './styles';
 
 interface Props extends RectButtonProps{
   title: string;
+  onPress: () => void;
 }
 
-export function Button({ title, ...rest }: Props){
+export function Button({
+  title,
+  onPress,
+  ...rest
+}: Props){
   return (
-    <Container {...rest}>
+    <Container onPress={onPress} {...rest}>
       <Title>
         { title }
       </Title>
