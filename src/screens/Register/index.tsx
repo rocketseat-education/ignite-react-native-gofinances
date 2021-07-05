6d017b2ee914479943734468c7100d8d4120ef0d
Dalton Menezes <daltonmenezes@outlook.com>
2021-07-05 14:55:52 -0300

    Remover dados do AsyncStorage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -81,7 +81,7 @@ export function Register(){
       return Alert.alert('Selecione a categoria');
 
 
-    const data = {
+    const newTransaction = {
       name: form.name,
       amount: form.amount,
       transactionType,
@@ -89,9 +89,15 @@ export function Register(){
     }
 
     try {
-      
-      await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
 
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
       
     } catch (error) {
       console.log(error);
