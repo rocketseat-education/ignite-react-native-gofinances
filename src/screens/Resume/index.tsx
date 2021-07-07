5dcd54083d331d7ba4c2b0fe64abfb8eb6a86373
Dalton Menezes <daltonmenezes@outlook.com>
2021-07-07 15:42:25 -0300

    Listando os totais por categoria

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { HistoryCard } from '../../components/HistoryCard';
 
 import {
   Container,
   Header,
-  Title
+  Title,
+  Content,
 } from './styles';
 
+import { categories } from '../../utils/categories';
+
+interface TransactionData {
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;  
+}
+
+interface CategoryData {
+  key: string;
+  name: string;
+  total: string;
+  color: string;
+}
+
 export function Resume(){
+  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+  async function loadData() {
+    const dataKey = '@gofinances:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    const expensives = responseFormatted
+    .filter((expensive: TransactionData) => expensive.type === 'negative');
+
+    const totalByCategory: CategoryData[] = [];
+
+    categories.forEach(category => {
+      let categorySum = 0;
+
+      expensives.forEach((expensive: TransactionData) => {
+        if(expensive.category === category.key){
+          categorySum += Number(expensive.amount);
+        }
+      });
+
+      if(categorySum > 0){
+        const total = categorySum
+        .toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })
+
+
+        totalByCategory.push({
+          key: category.key,
+          name: category.name,
+          color: category.color,
+          total,
+        });
+      }
+    });
+
+    setTotalByCategories(totalByCategory);
+  }
+
+  useEffect(() => {
+    loadData();
+  },[]);
+
+
   return(
     <Container>
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
-      
 
-      <HistoryCard
-        title="Compras"
-        amount="R$ 150,50"
-        color="red"
-      />
+      <Content>
+        {
+          totalByCategories.map(item => (
+            <HistoryCard
+              key={item.key}
+              title={item.name}
+              amount={item.total}
+              color={item.color}
+            />
+          ))
+        }
+      </Content>
 
     </Container>
   )
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -22,3 +22,8 @@ export const Title = styled.Text`
   font-size: ${RFValue(18)}px;
   color: ${({ theme }) => theme.colors.shape};
 `;
+
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: { flex: 1, padding: 24 }
+})``;
