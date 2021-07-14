a69cb0bcf397aa9ce11d4e6216feaafb5b390a4d
Dalton Menezes <daltonmenezes@outlook.com>
2021-07-14 17:01:52 -0300

    Dados sensiveis

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -5,6 +5,9 @@ import React, {
   useState
 } from 'react';
 
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
+
 import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
@@ -37,15 +40,13 @@ function AuthProvider({ children }: AuthProviderProps ) {
 
   async function signInWithGoogle() {
     try {
-      const CLIENT_ID = 'SEU_CLIENT_ID';
-      const REDIRECT_URI = 'SUA_REDIRECT_URI';
       const RESPONSE_TYPE = 'token';
       const SCOPE = encodeURI('profile email');
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
       const { type, params } = await AuthSession
-      .startAsync({ authUrl }) as AuthorizationResponse;
+        .startAsync({ authUrl }) as AuthorizationResponse;
       
       if (type === 'success') {
         const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
